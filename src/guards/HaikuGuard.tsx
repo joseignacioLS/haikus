8f044ade6bac1a0bbe95b962a7b5b48cc8a9ee69
Jose <jlabella@emeal.nttdata.com>
2025-03-31 13:53:36 +0200

    update guard

--- src/guards/HaikuGuard.tsx
+++ src/guards/HaikuGuard.tsx
@@ -1,14 +1,23 @@
 import { navigate } from "astro:transitions/client";
 import { useStore } from "@nanostores/react";
 import { data } from "../store/Data";
+import { useEffect } from "react";
 
 export default function HaikuGuard({ id }: { id: string }) {
   const haikus = useStore(data);
-  const visibleHaikus = haikus.filter((h) => h.show);
-  if (id === "random") {
-    const randomIndex = Math.floor(Math.random() * visibleHaikus.length);
-    navigate(`${import.meta.env.BASE_URL}${visibleHaikus[randomIndex].id}`);
-  }
+
+  useEffect(() => {
+    if (haikus.length === 0) return;
+    const visibleHaikus = haikus.filter((h) => h.show);
+    if (id === "random") {
+      const randomIndex = Math.floor(Math.random() * visibleHaikus.length);
+      navigate(`/${visibleHaikus[randomIndex].id}`);
+      return;
+    }
+    if (!visibleHaikus.find((h) => h.id === Number(id))) {
+      navigate("/");
+    }
+  }, [id, haikus]);
 
   return <></>;
 }
