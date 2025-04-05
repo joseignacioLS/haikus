233f9bd28f086bc8bc2bc9878548d8d7487ed993
Jose <ls.joseignacio@gmail.com>
2025-04-05 17:42:51 +0200

    make collections not warp

--- src/components/CollectionsCard.tsx
+++ src/components/CollectionsCard.tsx
@@ -1,28 +1,33 @@
 import { navigate } from "astro:transitions/client";
 import { useHaikuStore } from "../hooks/useHaikuStore";
+import { ERequestStatus } from "../types";
 import styles from "./CollectionsCard.module.scss";
+import { Spinner } from "./Spinner";
 import { TitledBlock } from "./TitledBlock";
 
 export const CollectionsCard = () => {
-  const { collections } = useHaikuStore();
+  const { status, collections } = useHaikuStore();
 
   return (
     <TitledBlock title={<h2>Colecciones</h2>}>
-      <div className={styles.tagList}>
-        {collections.map((tag) => {
-          return (
-            <button
-              key={tag}
-              className="rounded"
-              onClick={() => {
-                navigate(`/collection/${tag}`);
-              }}
-            >
-              {tag}
-            </button>
-          );
-        })}
-      </div>
+      {status === ERequestStatus.LOADING && <Spinner />}
+      {status === ERequestStatus.SUCCESS && (
+        <div className={styles.tagList}>
+          {collections.map((tag) => {
+            return (
+              <button
+                key={tag}
+                className="rounded"
+                onClick={() => {
+                  navigate(`/collection/${tag}`);
+                }}
+              >
+                {tag}
+              </button>
+            );
+          })}
+        </div>
+      )}
     </TitledBlock>
   );
 };
