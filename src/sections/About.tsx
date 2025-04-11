feb260b1d1b1f2274ccd74ae359e085fb4d7fa0f
Jose <jlabella@emeal.nttdata.com>
2025-04-11 14:14:17 +0200

    add link to seasonsclock

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,16 +4,17 @@ import styles from "./About.module.scss";
 export const About = () => {
   return (
     <div className={styles.body}>
-      <img
-        src={`/favicon.jpg`}
-        alt="Avatar del autor de la página"
-        style={{
-          viewTransitionName: "avatar-img",
-        }}
+      <button
         onClick={() => {
           navigate("/seasonsclock");
         }}
-      />
+        className={`naked ${styles.buttonImage}`}
+        style={{
+          viewTransitionName: "avatar-img",
+        }}
+      >
+        <img src={`/favicon.jpg`} alt="Avatar del autor de la página" />
+      </button>
       <div style={{ viewTransitionName: "about-text" }}>
         <p>
           ¡Hola! Me llamo Jose, y has entrado en mi diario de haikus,
