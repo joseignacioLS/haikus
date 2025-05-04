fcffe639fbde38fd2cdb2e98245c3b7033e3247a
Jose <ls.joseignacio@gmail.com>
2025-05-04 16:26:31 +0200

    add more vertical swipe directions

--- src/components/structure/Swipeable.tsx
+++ src/components/structure/Swipeable.tsx
@@ -1,13 +1,15 @@
-import { useState, type ReactNode } from "react";
+import { useState, type ReactElement } from "react";
 
 export const Swipeable = ({
   children,
   handleSwipe,
   className,
+  threshold = 50,
 }: {
-  children: ReactNode;
-  handleSwipe: (direction: "Right" | "Left") => void;
+  children: ReactElement;
+  handleSwipe: (direction: "Right" | "Left" | "Up" | "Down") => void;
   className?: string;
+  threshold?: number;
 }) => {
   const [touchStart, setTouchStart] = useState<
     { x: number; y: number } | undefined
@@ -21,14 +23,21 @@ export const Swipeable = ({
     const deltaY = touchEnd.y - touchStart.y;
     setTouchStart(undefined);
     setTouchEnd(undefined);
-    if (Math.abs(deltaY) > 50) return;
-    if (Math.abs(deltaX) < 50) return;
-    if (deltaX > 0) {
+    if (Math.abs(deltaY) > threshold && Math.abs(deltaX) > threshold) return;
+    if (deltaX > threshold) {
       handleSwipe("Left");
       return;
     }
-    if (deltaX < 0) {
+    if (deltaX < -threshold) {
       handleSwipe("Right");
+      return;
+    }
+    if (deltaY > threshold) {
+      handleSwipe("Up");
+      return;
+    }
+    if (deltaY < -threshold) {
+      handleSwipe("Down");
       return;
     }
   };
