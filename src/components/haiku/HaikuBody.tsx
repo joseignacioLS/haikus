4cf9ee6909ee439e56db62e95257a8bc36cedaf0
Jose <jlabella@emeal.nttdata.com>
2025-05-07 08:59:51 +0200

    adjust font size based on max haiku length

--- src/components/haiku/HaikuBody.tsx
+++ src/components/haiku/HaikuBody.tsx
@@ -1,14 +1,32 @@
+import { useHaikuStore } from "@/hooks/useHaikuStore";
+import { useEffect, useRef, useState } from "react";
 import styles from "./HaikuBody.module.scss";
 
 export const HaikuBody = ({ id, haiku }: { id?: number; haiku: string[] }) => {
+  const [fontSize, setFontSize] = useState(0);
+  const { maxHaikuLineLength } = useHaikuStore();
+  const bodyRef = useRef(null);
+
   const cleanLine = (line: string) => {
     return line.replace(/-/g, "").replace(/_/g, " ");
   };
+
+  useEffect(() => {
+    if (!maxHaikuLineLength) return;
+    const { offsetWidth } = bodyRef.current as any;
+    setFontSize((2.5 * offsetWidth) / maxHaikuLineLength);
+  }, [maxHaikuLineLength]);
+
   return (
-    <div className={`${styles.haiku}`} id={String(id)}>
-      {haiku.map((l) => {
-        return <p key={l}>{cleanLine(l)}</p>;
-      })}
+    <div ref={bodyRef} className={`${styles.haiku}`} id={String(id)}>
+      {fontSize > 0 &&
+        haiku.map((l) => {
+          return (
+            <p key={l} style={{ fontSize }}>
+              {cleanLine(l)}
+            </p>
+          );
+        })}
     </div>
   );
 };
--- src/hooks/useHaikuStore.tsx
+++ src/hooks/useHaikuStore.tsx
@@ -15,18 +15,23 @@ export const useHaikuStore = () => {
     collections: THaiku["tags"];
     date: Temporal.PlainDate;
     selected: THaiku["id"];
+    maxHaikuLineLength: number;
   }>({
     haikus: [],
     collections: [],
     date: Temporal.Now.plainDateISO(),
     selected: 1,
+    maxHaikuLineLength: 0,
   });
 
   useEffect(() => {
     const subscriptions = [
       haikusStore.subscribe((haikus) => {
+        const maxHaikuLineLength = haikus.reduce((acc, curr) => {
+          return Math.max(acc, ...curr.text.map((l) => l.length));
+        }, 0);
         setData((oldState) => {
-          return { ...oldState, haikus: [...haikus] };
+          return { ...oldState, haikus: [...haikus], maxHaikuLineLength };
         });
       }),
       collectionsStore.subscribe((collections) => {
