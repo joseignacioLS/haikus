0d5f8535fb9803409bb7eeda5a49a099f3bf50bf
Jose <jlabella@emeal.nttdata.com>
2025-05-15 11:10:27 +0200

    remove unnecessary hook

--- src/sections/DetailedHaiku.tsx
+++ src/sections/DetailedHaiku.tsx
@@ -1,12 +1,10 @@
 import { HaikuFullPage } from "@/components/haiku/HaikuFullPage";
 import { Spinner } from "@/components/notifications/Spinner";
 import { type THaiku } from "@/types";
-import { useHaikuGuard } from "@hooks/useHaikuGuard";
 import { useHaikuStore } from "@hooks/useHaikuStore";
 
 export default function DetailedHaiku({ id }: { id: THaiku["id"] }) {
   const { haikus } = useHaikuStore();
-  useHaikuGuard(id);
 
   const haiku = haikus.find((h) => h.id === id);
 
